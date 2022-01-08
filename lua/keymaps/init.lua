b68c9871a1573d9c662daf60f9bcc2a82cf9cc9f
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-01-08 19:10:34 +0600

    Formatting of my keymaps config

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,28 +1,27 @@
 -- Leader key is used with other keys to make new unique maps
 -- My leader key is space
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 ------------------- Keymaps ----------------------
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = false }
 
 -- Switch to diffents splits faster
-map('n', '<C-h>', '<C-w>h', opts)
-map('n', '<C-l>', '<C-w>l', opts)
-map('n', '<C-k>', '<C-w>k', opts)
-map('n', '<C-j>', '<C-w>j', opts)
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-j>", "<C-w>j", opts)
 
 -- Quit insert mode faster
-map('i', '<A-j>', '<Esc>', { noremap = true, silent = true } )
+map("i", "<A-j>", "<Esc>", { noremap = true, silent = true })
 
 -- Better indetation
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- I want to save files which I have no access to write
-map('c', 'w!!', '%!sudo tee > /dev/null %', {})
+map("c", "w!!", "%!sudo tee > /dev/null %", {})
 
 -- Lsp keymaps for better IDE experience
-map('n', '<C-u>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>", {})
-map('n', '<C-d>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", {})
-
+map("n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>", {})
+map("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", {})
