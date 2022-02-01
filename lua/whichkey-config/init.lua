00b6f495ea71a7add0b3f54af44031abd56224bf
RitchielRez <ritchielrez@gmail.com>
2022-02-01 13:15:54 +0600

    Bit of fixing

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -1,53 +1,53 @@
-local wk = require('which-key')
+local wk = require("which-key")
 local leader_mappings = {
-    q = {":q!<cr>", "Quit"},
-    Q = {":wq!<cr>", "Save and Quit"},
-    ww = {":w!<cr>", "Save"},
-    x = {":bdelete!<cr>", "Close Buffer"},
-    n = {":bnext!<cr>", "Next buffer"},
-    m = {":bprevious!<cr>", "Previous buffer"},
-    e = {":NvimTreeToggle<cr>", "Toggle file explorer"},
-    f = {
-        name = "Telescope",
-        f = {":Telescope find_files<cr>", "Find files"},
-        r = {":Telescope live_grep<cr>", "Live grep"},
-        b = {":Telescope buffers<cr>", "Buffers"},
-        o = {":Telescope oldfiles<cr>", "Recent files"}
-    },
-
+	q = { ":q!<cr>", "Quit" },
+	Q = { ":wq!<cr>", "Save and Quit" },
+	ww = { ":w!<cr>", "Save" },
+	x = { ":bdelete!<cr>", "Close Buffer" },
+	n = { ":bnext!<cr>", "Next buffer" },
+	m = { ":bprevious!<cr>", "Previous buffer" },
+	e = { ":NvimTreeToggle<cr>", "Toggle file explorer" },
+	f = {
+		name = "Telescope",
+		f = { ":Telescope find_files<cr>", "Find files" },
+		r = { ":Telescope live_grep<cr>", "Live grep" },
+		b = { ":Telescope buffers<cr>", "Buffers" },
+		o = { ":Telescope oldfiles<cr>", "Recent files" },
+	},
 }
 
-local opts = { prefix = '<leader>' }
+local opts = { prefix = "<leader>" }
 wk.register(leader_mappings, opts)
 
 local comment_mappings = {
-    gc = {
-        name = "Comment",
-        c = {":CommentToggle<cr>", "Comment/Uncomment lines"}
-    }
+	gc = {
+		name = "Comment",
+		c = { ":CommentToggle<cr>", "Comment/Uncomment lines" },
+	},
 }
 
 wk.register(comment_mappings, { mode = "n", prefix = "", preset = true })
 wk.register(comment_mappings, { mode = "v", prefix = "", preset = true })
 
 local lsp_mappings = {
-    rn = {"<cmd>lua vim.lsp.buf.rename()<CR>", "Lsp: Symbol rename"},
-    ca = {"<cmd>lua vim.lsp.buf.code_action()<CR>", "Lsp: Code action"};
-    K = {"<cmd>lua vim.lsp.buf.hover()<CR>", "Lsp: Hover keyword"},
-    gd = {"<cmd>lua vim.lsp.buf.definition()<CR>", "Lsp: Go to Definition"},
-    go = {"<cmd>lua vim.diagnostic.open_float()<CR>", "Lsp: Line diagnostic"},
-    gj = {"<cmd>lua vim.diagnostic.goto_next()<CR>", "Lsp: Jump to next diagnostic"},
-    gk = {"<cmd>lua vim.diagnostic.goto_prev()<CR>", "Lsp: Jump to previous diagnostic"},
+	rn = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Lsp: Symbol rename" },
+	ca = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Lsp: Code action" },
+	K = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Lsp: Hover keyword" },
+	gd = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Lsp: Go to Definition" },
+	go = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Lsp: Line diagnostic" },
+	gj = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Lsp: Jump to next diagnostic" },
+	gk = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Lsp: Jump to previous diagnostic" },
 }
 
 wk.register(lsp_mappings, { mode = "n", prefix = "", preset = true })
 
 local quickfix_mappings = {
-    c = {
-        j = {"<cmd>cnext<CR>", "Quickfix: Next in the list"},
-        k = {"<cmd>cprev<CR>", "Quickfix: Previous in the list"},
-        q = {"<cmd>cclose<CR>", "Quickfix: Close the list"},
-    }
+	c = {
+        name = "Quickfix",
+		j = { "<cmd>cnext<CR>", "Next in the list" },
+		k = { "<cmd>cprev<CR>", "Previous in the list" },
+		c = { "<cmd>cclose<CR>", "Close the list" },
+	},
 }
 
-wk.register(quickfix_mappings, { mode = "n", prefix = "", preset = true})
+wk.register(quickfix_mappings, { mode = "n", prefix = "<leader>" })
