7f80bba46547347937245d1dbd909b6a4a1b4d44
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-13 09:00:48 +0600

    Formatted my statusline config

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -107,31 +107,31 @@ local function lineinfo()
 	return "%#WildMenu# %P %l:%c "
 end
 local vcs = function()
-  local git_info = vim.b.gitsigns_status_dict
-  if not git_info or git_info.head == "" then
-    return ""
-  end
-  local added = git_info.added and ("%#GitSignsAdd# +" .. git_info.added) or ""
-  local changed = git_info.changed and ("%#GitSignsChange# ~" .. git_info.changed) or ""
-  local removed = git_info.removed and ("%#GitSignsDelete# -" .. git_info.removed) or ""
-  if git_info.added == 0 then
-    added = ""
-  end
-  if git_info.changed == 0 then
-    changed = ""
-  end
-  if git_info.removed == 0 then
-    removed = ""
-  end
-  return table.concat {
-     " ",
-     added,
-     changed,
-     removed,
-     "%#GitSignsAdd#  ",
-     git_info.head,
-     " %#Normal#",
-  }
+	local git_info = vim.b.gitsigns_status_dict
+	if not git_info or git_info.head == "" then
+		return ""
+	end
+	local added = git_info.added and ("%#Macro# +" .. git_info.added) or ""
+	local changed = git_info.changed and ("%#Identifier# ~" .. git_info.changed) or ""
+	local removed = git_info.removed and ("%#Error# -" .. git_info.removed) or ""
+	if git_info.added == 0 then
+		added = ""
+	end
+	if git_info.changed == 0 then
+		changed = ""
+	end
+	if git_info.removed == 0 then
+		removed = ""
+	end
+	return table.concat({
+		" ",
+		added,
+		changed,
+		removed,
+		" %#Search#  ",
+		git_info.head,
+		" %#Normal#",
+	})
 end
 
 Statusline = {}
@@ -146,7 +146,7 @@ Statusline.active = function()
 		filename(),
 		"%#Normal#",
 		lsp(),
-        vcs(),
+		vcs(),
 		"%=%#StatusLineExtra#",
 		lineinfo(),
 	})
